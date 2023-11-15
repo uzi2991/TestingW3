f762ea30fbcb22e53953257dc1769bcc53450297
uzi2991 <minhnghiacan25032003@gmail.com>
2023-11-15 18:10:53 +0700

    Add back up twitter account

--- project/constant.py
+++ project/constant.py
@@ -1,3 +1,7 @@
 DRIVER_PATH = "D:/chromedriver-win64/chromedriver.exe"
 TWITTER_USERNAME = "WSelenium7972"
 TWITTER_PASSWORD = "1mCoFL2RmYujBS8"
+
+# back up account wselenium93@gmail.com
+# TWITTER_USERNAME = "WSelenium88065"
+# TWITTER_PASSWORD = "1mCoFL2RmYujBS8"
